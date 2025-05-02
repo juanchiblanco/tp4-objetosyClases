de8826fcd1e06824ae6fc10dda853382f5099610
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-02 19:37:21 -0300

    cree tres instancias del producto, los agregue a un array y con la funcion imprimir datos los mostre en panatalla

--- js/04-ejercicio.js
+++ js/04-ejercicio.js
@@ -5,11 +5,28 @@ class Producto{
         this.precio = precio}
 
         imprimeDatos(){
-            document.writeln(`<h3>Producto</h3>`)
+            document.writeln(`<h3>Producto: ${this.nombre}</h3>`)
                document.writeln(`<ul>
-                <li>Nombre: ${this.nombre}</li>
-                <li>Precio: ${this.precio}</li>
+                <li>Precio: $${this.precio}</li>
                 <li>Codigo: ${this.codigo}</li>
                 </ul>`)
         }
     }
+
+    mostazas = []
+
+    const mostazaSavora = new Producto('150', 'Mostaza Savora', '1899,99')
+    mostazas.push(mostazaSavora)
+    const mostazaDanica = new Producto('151', 'Mostaza Danica', '1699,99')
+    mostazas.push(mostazaDanica)
+    const mostazaNatura = new Producto('152', 'Mostaza Natura', '1399,99')
+    mostazas.push(mostazaNatura)
+
+    console.log(mostazas)
+
+    document.writeln('<h2>Seccion Mostazas</h2>')
+    document.writeln(`<p>Cantidad: ${mostazas.length}</p>`)
+    
+    mostazaSavora.imprimeDatos()
+    mostazaDanica.imprimeDatos()
+    mostazaNatura.imprimeDatos()
