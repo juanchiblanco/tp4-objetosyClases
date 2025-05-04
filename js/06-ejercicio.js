fe835661371f9803b7ac4ce7bb1baaeffe4f1095
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-04 19:44:06 -0300

    cree tres objetos con la clase libro

--- js/06-ejercicio.js
+++ js/06-ejercicio.js
@@ -19,4 +19,17 @@ class Libro {
     set setTitulo(nuevoTitulo){return this.#titulo = nuevoTitulo}
     set setAutor(nuevoAutor){return this.#autor = nuevoAutor}
     set setNumPaginas(nuevoNumPaginas){return this.#numPaginas = nuevoNumPaginas}
+
+    mostrarLibro(){
+        document.writeln(`<p>El libro ${this.#titulo} con ISBN ${this.#isbn} creado por el autor ${this.#autor} tiene ${this.#numPaginas} páginas</p>`)
+    }
+
 }
+
+const elExtranjero = new Libro ('9787856412546', 'El Extranjero', 'Albert Camus', 128)
+const laGayaCiencia = new Libro ('9745125489654', 'La Gaya Ciencia', 'Friedrich Nietzsche', 256)
+const aion = new Libro ('9620154879254', 'Aion', 'Carl Gustav Jung', 512)
+
+elExtranjero.mostrarLibro()
+laGayaCiencia.mostrarLibro()
+aion.mostrarLibro()
