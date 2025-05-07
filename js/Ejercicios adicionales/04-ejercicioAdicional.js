cb7372c9eda82d1772f02bc8967d828b57e0fb54
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-07 18:10:19 -0300

    ejercicio adicional 4 complatdo

--- js/Ejercicios adicionales/04-ejercicioAdicional.js
+++ js/Ejercicios adicionales/04-ejercicioAdicional.js
@@ -3,22 +3,52 @@ const alumno = {
   curso: prompt("Ingresa el curso donde se encuentra"),
   calificaciones: [],
 
-  pushNotas: function(){
-    do{
-        let nota= prompt("Ingresa la nota del alumno")
-        this.calificaciones.push(nota)
-    }while(confirm('Quieres agregar otra nota?'))},
+  pushNotas: function () {
+    do {
+      let nota = parseInt(prompt("Ingresa la nota del alumno"));
+      if ((nota <= 10) & (nota >= 0)) {
+        this.calificaciones.push(nota);
+      }
+    } while (confirm("Quieres agregar otra nota?"));
+  },
 
-    notaMasAlta: function(){
-        let notaMax = 0
-        for(i=0;i<this.calificaciones.length;i++){
-            if(this.calificaciones[i]>notaMax){notaMax=this.calificaciones[i]}
-        }
-        return notaMax
-    },
-};
+  notaMasAlta: function () {
+    let notaMax = 0;
+    for (i = 0; i < this.calificaciones.length; i++) {
+      if (this.calificaciones[i] > notaMax) {
+        notaMax = this.calificaciones[i];
+      }
+    }
+    return notaMax;
+  },
 
-alumno.pushNotas()
-alumno.notaMasAlta()
-document.writeln(`La nota mas alta de ${alumno.nombre} es: ${alumno.notaMasAlta()}`)
-console.log(alumno.calificaciones)
+  clasificarAlumno: function () {
+    let clasificacion = "";
+
+    switch (true) {
+        case this.notaMasAlta() >= 9:
+          clasificacion = "A";
+          break;
+        case this.notaMasAlta() >= 7:
+          clasificacion = "B";
+          break;
+        case this.notaMasAlta() >= 5:
+          clasificacion = "C";
+          break;
+        default:
+          clasificacion = "D";
+      }
+    
+      return clasificacion;
+    }
+  }
+
+alumno.pushNotas();
+alumno.notaMasAlta();
+alumno.clasificarAlumno();
+document.writeln(
+  `La nota mas alta de ${
+    alumno.nombre
+  } es: ${alumno.notaMasAlta()} (${alumno.clasificarAlumno()})`
+);
+console.log(alumno.calificaciones);
