c7d4cac77f914b94546f6fdeaed05d90fe92664c
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-07 10:12:46 -0300

    ejercicio extra 1 completado con solo objeto

--- js/Ejercicios adicionales/01-ejercicioAdicional.js
+++ js/Ejercicios adicionales/01-ejercicioAdicional.js
@@ -1,33 +1,27 @@
-class Persona{
-    constructor(nombre,edad,hobbies){
-        this.nombre=nombre
-        this.edad=edad
-        this.hobbies = []
-    }
+const persona = {
+    nombre:prompt('Ingresa tu nombre'),
+    edad:parseInt(prompt('ingresa tu edad')),
+    hobbies: [],
 
-    extraerHobbies(){
-        do{
-           let hobbieUsuario = prompt('Ingresa un hobbie que tengas')
-           this.hobbies.push(hobbieUsuario)
-        }
-        while(confirm(`Deseas ingresar otro hobbie?`))
-    
-    }
-
-    mostrarDatos(){
-        document.writeln(`<h2>${this.nombre}</h2>`)
-        document.writeln(`<p>Edad: ${this.edad}</p>`)
-        document.writeln('<h3>Hobbies</h3>')
-        document.writeln('<ul>')
-        for(let i=0;i<this.hobbies.length;i++){
-            document.writeln(`<li>${this.hobbies[i]}</li>`)
-        }
-        document.writeln('</ul>')}
+    extraerHobbies:function(){
+                do{
+                   let hobbieUsuario = prompt('Ingresa un hobbie que tengas')
+                   this.hobbies.push(hobbieUsuario)
+                }
+                while(confirm(`Deseas ingresar otro hobbie?`))
+            
+            },
+            
+            mostrarDatos:function(){
+                        document.writeln(`<h2>${this.nombre}</h2>`)
+                        document.writeln(`<p>Edad: ${this.edad}</p>`)
+                        document.writeln('<h3>Hobbies</h3>')
+                        document.writeln('<ul>')
+                        for(let i=0;i<this.hobbies.length;i++){
+                            document.writeln(`<li>${this.hobbies[i]}</li>`)
+                        }
+                        document.writeln('</ul>')}
 }
 
-const nombre = prompt('Ingresa tu nombre')
-const edad = parseInt(prompt('Ingresa tu edad'))
-
-const usuario = new Persona(nombre,edad)
-usuario.extraerHobbies()
-usuario.mostrarDatos()
+persona.extraerHobbies()
+persona.mostrarDatos()
