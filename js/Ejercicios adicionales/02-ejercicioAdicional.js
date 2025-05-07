e6ab760cfb853c642e3f3d455eb5598016fb4ea5
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-07 10:23:03 -0300

    cree objetos y array de objetos

---
+++ js/Ejercicios adicionales/02-ejercicioAdicional.js
@@ -0,0 +1,11 @@
+const estudiantes = []
+
+const juan = {nombre:'Juan',nota:4}
+const lucas = {nombre:'Lucas',nota:5}
+const guadalupe = {nombre:'Guadalupe',nota:10}
+const lucia = {nombre:'Lucia',nota:7}
+const jorge = {nombre:'Jorge',nota:8}
+const edith = {nombre:'Edith',nota:9}
+
+estudiantes.push(juan,lucas,guadalupe,lucia,jorge,edith)
+console.log(estudiantes)
