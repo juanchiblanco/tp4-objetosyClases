092eff57d9c11397705bb1de0b4d1f010ffb03b2
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-07 10:47:38 -0300

    ejercicio extra2 completado

--- js/Ejercicios adicionales/02-ejercicioAdicional.js
+++ js/Ejercicios adicionales/02-ejercicioAdicional.js
@@ -9,3 +9,29 @@ const edith = {nombre:'Edith',nota:9}
 
 estudiantes.push(juan,lucas,guadalupe,lucia,jorge,edith)
 console.log(estudiantes)
+
+const notas = estudiantes.map(objeto => objeto.nota)
+const nombres = estudiantes.map(objeto => objeto.nombre)
+
+function promedio(){
+    let suma = 0;
+    for (let i = 0; i < notas.length; i++) {
+      suma += notas[i];
+    }
+    const resultado = suma / notas.length
+    document.writeln(`La nota promedio es ${resultado}`)
+}
+
+function mostrarDatos(){
+    document.writeln('<ul>')
+    for(let i=0;i<estudiantes.length;i++){
+        document.writeln(`<li>${nombres[i]}: ${notas[i]}</li>`)
+    }
+    document.writeln('</ul>')
+}
+
+document.writeln('<h2>Notas</h2>')
+
+mostrarDatos()
+promedio()
+
